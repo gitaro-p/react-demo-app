9bfaf30bd61819da15c39838d6a1b28ca925c13a
清川　裕太郎 <yuu7353@gmail.com>
2020-11-16 21:20:19 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsB9_8BTDB51xqRUUDtc-bYVUZhXoxgY4',
+  authDomain: 'idobata-98af4.firebaseapp.com',
+  databaseURL: 'https://idobata-98af4.firebaseio.com',
+  projectId: 'idobata-98af4',
+  storageBucket: 'idobata-98af4.appspot.com',
+  messagingSenderId: '664424235803',
+  appId: '1:664424235803:web:829d44ae08c66c306c09be'
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef = database.ref('messages');
+
+export const pushMessage = ({ name, text }) => {
+  messageRef.push({ name, text });
+};
